8f279043a216be98dd970309487da2ade1740dd7
Kahoru Hirai <>
2020-06-12 09:37:31 +0900

    Updates

---
+++ precache-manifest.d0289186ccd8b664de21eba9fede2421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56141a2ca7a946fe8ef3e652dc190c6",
+    "url": "/reactjs-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "9de72754778ee71cf11c",
+    "url": "/reactjs-tic-tac-toe/static/css/main.a1b613fc.chunk.css"
+  },
+  {
+    "revision": "851366f05ad10764b1cf",
+    "url": "/reactjs-tic-tac-toe/static/js/2.440ae3e6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactjs-tic-tac-toe/static/js/2.440ae3e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de72754778ee71cf11c",
+    "url": "/reactjs-tic-tac-toe/static/js/main.db8ec17b.chunk.js"
+  },
+  {
+    "revision": "7584172e7044ba5a6f7c",
+    "url": "/reactjs-tic-tac-toe/static/js/runtime-main.8276156e.js"
+  }
+]);
